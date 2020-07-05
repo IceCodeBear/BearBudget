54c9a7c77e432ed5dbf9fd1f4473640b551a2838
Jie Wu <jiewu97@gmail.com>
2020-07-05 00:23:26 -0400

    Added 404 page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-import React from 'react';
-import './App.css';
-import * as firebase from 'firebase'
+import React from "react";
+import "./App.css";
 
 class App extends React.Component {
-
   constructor() {
     super();
     this.state = {
-      speed: 10
-    }
+      speed: 10,
+    };
   }
 
-  componentDidMount() {
-    const rootRef = firebase.database().ref()
-    const speedRef = rootRef.child('speed')
-    speedRef.on('value', snap => {
-      this.setState({
-        speed: snap.val()
-      });
-    });
-  }
+  // componentDidMount() {
+  //   const rootRef = firebase.database().ref();
+  //   const speedRef = rootRef.child("speed");
+  //   speedRef.on("value", (snap) => {
+  //     this.setState({
+  //       speed: snap.val(),
+  //     });
+  //   });
+  // }
 
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         {/* <h1>{this.state.speed}</h1> */}
         <h1>Work in Progress</h1>
       </div>
-    )
+    );
   }
 }
 

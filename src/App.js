07033fae2d01aa8b128aaef8e0ac21cfea09b2e8
Jie Wu <jiewu97@gmail.com>
2020-07-09 00:34:18 -0400

    Minor fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
 import React from "react";
 import "./App.css";
-const firebase = require('firebase/app');
-require('firebase/database');
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      speed: 10,
-    };
-  }
-
-  componentDidMount() {
-    const rootRef = firebase.database().ref();
-    const speedRef = rootRef.child("speed");
-    speedRef.on("value", (snap) => {
-      this.setState({
-        speed: snap.val(),
-      });
-    });
-  }
 
   render() {
     return (

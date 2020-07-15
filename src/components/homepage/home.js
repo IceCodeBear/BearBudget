fb48edfce00001aa99398334df53300e37a9f6c1
Jie Wu <jiewu97@gmail.com>
2020-07-14 22:37:55 -0400

    Added WIP messge

--- src/components/homepage/home.js
+++ src/components/homepage/home.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { Container, Header } from "semantic-ui-react";
+import { Container, Header, Message } from "semantic-ui-react";
 
 const Home = () => (
+
     <div className="App-header">
+        <Message
+            size='mini'
+            header='Work in Progress'
+            content='This website is still in the development stages.'
+        />
+
         <Container text>
             <Header as="h1">Welcome to BearBudget</Header>
             <Header as="h2">Budget The{" "}

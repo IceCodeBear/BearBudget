3470779b4e2b90ec1e7c85b7dc770b63ef61a9d9
Jie Wu <jiewu97@gmail.com>
2020-10-20 18:45:56 -0400

    Minor fixes

---
+++ src/components/calculator/calculator.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container, Header, Message } from "semantic-ui-react";
+
+const calculator = () => (
+
+    <div className="App-header">
+
+
+        <Container text>
+            <Header as="h1">Retirement Calculator</Header>
+            <Header as="h2"></Header>
+
+        </Container>
+    </div>
+);
+
+export default calculator;
